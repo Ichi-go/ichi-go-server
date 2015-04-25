4e93b32aab660c19c4387efcd9c758fac55347cd
Izzy Cecil <lorr.cecil@gmail.com>
2015-04-25 16:42:37 -0600

    ADD: Main now routs filters correctly.

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,7 @@ module Main where
 import           Control.Applicative
 import           Control.Monad.IO.Class
 import           Data.ByteString.Char8
+import           Data.Maybe
 
 import		 Data.Aeson
 import           Snap.Core
@@ -17,24 +18,44 @@ import           EventData
         
 main :: IO ()
 main = do
-	initDB "events" --Initializes the DB everytime. We want to change this
-	quickHttpServe site
+  Prelude.putStr "Weclome to 一期一会!"
+  Prelude.putStr "Initializing Database..."
+  initDB "events" --Initializes the DB everytime. We want to change this
+  quickHttpServe site
 
 {-| Serves the Ichi-Go web site -}
 site :: Snap ()
 site =
-    ifTop (writeText "Weclome to 一期一会!") <|>
-    route [ ("getEvents", eventRequestHandler),
-            ("addEvent", addEventHandler) 
-          ]
+  ifTop (writeText "Weclome to 一期一会!") <|>
+  route [ ("getEvents", eventRequestHandler),
+          ("addEvent", addEventHandler) 
+        ]
 
           
 {-| Returns all requested events as JSON -}          
 eventRequestHandler :: Snap ()
 eventRequestHandler = do
-  events <- liftIO $ queryEvents  "events"
+  bfilter <- filter
+  events  <- liftIO $ queryEvents "events" bfilter
   maybe (writeText "Database error!")
         writeJSON $ Just events
+  where
+    filter = do
+      rawLat <- getPostParam "lat"
+      rawLon <- getPostParam "lon"
+      logError $ fromMaybe "???" rawLat
+      logError $ fromMaybe "???" rawLon
+      -- logError "Post from LAT: " ++ (fromMaybe "???" rawLat) ++" LON: " ++ (fromMaybe "???" rawLon)
+      return $ makeEventFilter rawLat rawLon
+
+{-| Returns a Maybe EventFilter from the QueryParams used in
+    eventRequestHandler. Right now has a magical number for radius. -}
+makeEventFilter :: Maybe ByteString -> Maybe ByteString -> Maybe EventFilter
+makeEventFilter rawLat rawLon = do
+  lat <- rawLat
+  lon <- rawLon
+  return $  EventFilter (read $ unpack lat) (read $ unpack lon) 0.3
+
 
 {-| Searches the body for an event in JSON and adds it to the DB -}
 addEventHandler :: Snap ()
